71c9af7924e35e8a7a2e3a10fa0e95d5cfdaedfa
eduardo rosa <eduardorosafp@gmail.com>
2023-05-03 15:54:17 -0300

    resolving errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Eduardo</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a front ende developer, building <br className='sm:block hidden' />
+            I'm a front end developer, building <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
